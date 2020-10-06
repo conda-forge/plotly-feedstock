687abbe58f59e64a3f663f050169db818f44345f
Jason K. Moore <moorepants@gmail.com>
2020-10-06 17:20:35 +0200

    Correct tarball name.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,4 @@ set -ex
 $PYTHON -m pip install . -vv
 npm pack jupyterlab-${PKG_NAME}@${PKG_VERSION}
 mkdir -p ${PREFIX}/share/jupyter/lab/extensions/js
-cp jupyterlab-${PKG_NAME}@${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
+cp jupyterlab-${PKG_NAME}-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
